5ec6dc56ced57bad98a706e69c721996e33593b6
qincy <353713885@qq.com>
2020-03-10 17:42:57 +0800

    first commit

---
+++ src/main/java/com/example/demo/pojo/Employee.java
@@ -0,0 +1,14 @@
+package com.example.demo.pojo;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+    private String id;
+    private String employeeUid;
+    private String employeeName;
+    private String gender;
+    private Integer age;
+    private int archive;
+
+}
