c9c07ef55aa130fab6c54bcfd6f65aea081935ec
malekouo <353713885@qq.com>
2020-03-15 23:50:45 +0800

    添加数据库

--- src/main/java/com/example/demo/controller/HelloDemo.java
+++ src/main/java/com/example/demo/controller/HelloDemo.java
@@ -1,14 +1,32 @@
 package com.example.demo.controller;
 
+import com.example.demo.mapper.EmployeeMapper;
+import com.example.demo.pojo.Employee;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HelloDemo {
+    @Autowired
+    private EmployeeMapper employeeMapper;
+
     @RequestMapping("/hello")
     @ResponseBody
     public String hello() {
         return "hello!";
     }
+
+    @RequestMapping("/testMapper")
+    @ResponseBody
+    public String mapper() {
+        Employee employeeInfo = employeeMapper.getEmployeeInfo("111111");
+        log.info("name{}",employeeInfo.getEmployeeName());
+        return "mapper!";
+    }
+
+
 }
--- src/main/java/com/example/demo/mapper/EmployeeMapper.java
+++ src/main/java/com/example/demo/mapper/EmployeeMapper.java
@@ -1,4 +1,9 @@
 package com.example.demo.mapper;
 
+import com.example.demo.pojo.Employee;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface EmployeeMapper {
+    Employee getEmployeeInfo(String employeeUid);
 }
